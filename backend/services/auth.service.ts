4eb323f08b35fa7a2533fd0dfa20d439d2100263
isabel64450 <isa.dumas64@gmail.com>
2026-09-15 10:02:19 +0200

    interface service correction

--- backend/services/auth.service.ts
+++ backend/services/auth.service.ts
@@ -8,16 +8,11 @@ import type { AuthRepository } from "../types/auth.repository.interface.js";
 
 
 
-
-interface AuthRepository {
-  getUserByEmail(userEmail: string): Promise<User | null>;
-  createUser(user: NewUser): Promise<number>;
-  saveResetToken(email: string, token: string, expire: Date): Promise<void>;
-  getUserByResetToken(token: string): Promise<User | null>;
-  updatePassword(userId: number, hashedPassword: string): Promise<void>;
 interface VerificationTokenPayload extends JwtPayload {
   id: number;
 }
+
+
 
 
 
